d9ed256165de055480b93de767a7601722ee32e6
Your Name <you@example.com>
2022-02-07 18:56:53 +0600

    opp

---
+++ S2.1/S2.1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    using namespace std;
+    //а) вычисления значения функции y=7x2+3x+6 при любом значении x;
+    /*double x;
+    cout << "Введите значение x: ";
+    cin >> x;
+    double y = (7 * pow(x, 2)) + 3 * x + 6;
+    cout << "y = " << y;*/
+
+    //б) вычисления значения функции x=12a2+7a+12 при любом значении а.
+    /*double a;
+    cout << "Введите значение x: ";
+    cin >> a;
+    double x = (12 * pow(a, 2)) + 7 * a + 12;
+    cout << "x = " << x; */
+}
